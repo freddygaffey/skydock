e0cffff931162ccfb22e853e5dd5676295ed4092
Fred <fredgaffey08@gmail.com>
2025-09-16 09:59:06 +1000

    add some more docs and fix loop

--- software/drone.py
+++ software/drone.py
@@ -2,8 +2,8 @@ import pymavlink
 from pymavlink import mavutil
 import math 
 import numpy as np
-# from get_cam_stream_and_run_ai import *
-# from get_weed_positon import *
+from scipy.spatial.transform import Rotation as R # this is needed for the q
+
 
  
 # I found the videos from Intelligent Quads helpfull 
@@ -17,27 +17,63 @@ import numpy as np
 class Drone_mvp():
     """this is a drone mvp will be renamed later to Drone"""
     
+    """for  quaternions I will use the [x, y, z, w] this is the convetoin for sicpy"""
+    
     def __init__(self):
         self.battery_capacity = 4800 # in mah
         path_to_uav = "/dev/ttyACM1"
         self.connection = mavutil.mavlink_connection(path_to_uav, baud=115200)
         self.connection.wait_heartbeat()
+        
         # defing what command to stream
         self.set_a_message_interval("BATTERY_STATUS",interval=1)
-        self.set_a_message_interval("GPS_RAW_INT",interval=10)
-        self.set_a_message_interval("SYS_STATUS",interval=10)
+        # self.set_a_message_interval("GPS_RAW_INT",interval=10)
+        # self.set_a_message_interval("SYS_STATUS",interval=20)
+        self.set_a_message_interval("GIMBAL_DEVICE_ATTITUDE_STATUS",interval=0.25)
+        self.set_a_message_interval("ATTITUDE_QUATERNION",interval=0.25)
+
         
-        # this will stream the rc chanels 
+        # this will stream the rc chanels thay are speshal
         self.connection.mav.request_data_stream_send(
             self.connection.target_system,
             self.connection.target_component,
             mavutil.mavlink.MAV_DATA_STREAM_RC_CHANNELS,  # stream for RC messages
             2,  # Hz
-            1    # start streaming
-        )
+            1)    # start streaming
         
+    def update():
+        self.batt_v = self.get_batt_v()
+        self.gimbal_attitude = self.get_gimbal_attitude()
+        self.batt_mah_left = self.get_batt_mah_left()
         
+    
+    
+    def get_gimbal_attitude(self):
+        """this uses QUETONIONS so BEWHERE"""
+        # TODO: check if this is the correct global q
+        
+        q_cam = None 
+        q_drone = None
+        while True:
+            msg = self.connection.recv_msg()  
+            if msg is not None:
+                msg = msg.to_dict()
+                if msg["mavpackettype"] == "GIMBAL_DEVICE_ATTITUDE_STATUS":
+                    q_cam = (msg["q"])
+                elif msg["mavpackettype"] == "ATTITUDE_QUATERNION":
+                    q_drone = (msg['q1'], msg['q2'], msg['q3'],msg['q4'])  # [x, y, z,w]
+                if q_cam != None and q_drone != None:
+                    # convert to a rotaion matrix 
+                    r_drone = R.from_quat(q_drone)
+                    r_cam = R.from_quat(q_cam)
 
+                    # work out global camera orientation
+                    r_global = r_drone * r_cam
+
+                    # Convert back to quaternion [w, x, y, z]
+                    q_global = r_global.as_quat()
+                    return q_global
+            
     def get_batt_v(self):
         """this code works by converitng the msg to a dict then filtering"""
         while True:
@@ -46,7 +82,7 @@ class Drone_mvp():
                 msg = msg.to_dict()
                 if msg["mavpackettype"] == "BATTERY_STATUS":
                     return msg["voltages"][0]/1000
-                
+
     def get_batt_mah_left(self):
         """this code works by converitng the msg to a dict then filtering"""
         while True:
@@ -58,10 +94,6 @@ class Drone_mvp():
 
                 
    
-
-
-
-
     def set_a_message_interval(self,message_name,interval=1):
         """interval in sec"""
         # https://mavlink.io/en/mavgen_python/howto_requestmessages.html
@@ -82,12 +114,12 @@ class Drone_mvp():
 
 class See():
     """this is a class of what the cammera sees"""
-    
+
     """see_input_dict = {
         "gps_poss":(x,y,z),
         "gps_num_of_sats": int(NUM),
-        
-        
+        "q_cam_rel_global": [x, y, z, w],
+        "ai_dict": SEE BELOW
         
         }"""
     
@@ -108,9 +140,7 @@ class See():
     
     def __init__(self,see_input_dict):
         
-        gps_poss, gps_num_of_sats, gimbel_angle, ai_dict
+        # gps_poss, gps_num_of_sats, gimbel_angle, ai_dict
         # this setup allow easer passing of files 
         
-        
-    # def __init__(self, gps_poss, gps_num_of_sats, gimbel_angle, ai_dict):
-        
+                
--- software/main.py
+++ software/main.py
@@ -1,5 +1,5 @@
 from drone import *
-from get_cam_stream_and_run_ai 
+# from get_cam_stream_and_run_ai 
 
 
 
@@ -7,4 +7,8 @@ from get_cam_stream_and_run_ai
 uav = Drone_mvp()
 print(uav.get_batt_v())
 print(uav.get_batt_mah_left())
+while 1:
+    print(uav.get_gimbal_attitude())
+
+
 
