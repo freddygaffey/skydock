0c85cc2b9b62c74194b4e7644f3fa12bea08d13b
Fred Gaffey <fredgaffey08@gmail.com>
2025-11-22 18:13:30 +1100

    added the move commands developed in skydock-gui

--- software/move.py
+++ software/move.py
@@ -1,13 +1,17 @@
-from telemetry import Telemetry, telm_singleton
+import threading
+from telemetry import telm_singleton
 import time
 
-class Move(Telemetry):
+class Move():
 
-    def __init__(self,telm_singleton.connection):
-        self.connection = drone_connection
-        self.move_mode = "STOP"
+    def __init__(self):
+        self.connection = telm_singleton.connection
         self.mode_mapping = {'STABILIZE': 0,'ACRO': 1,'ALT_HOLD': 2,'AUTO': 3,'GUIDED': 4,'LOITER': 5,'RTL': 6,'CIRCLE': 7,'OF_LOITER': 10,'DRIFT': 11,'SPORT': 13,'FLIP': 14,'AUTOTUNE': 15,'POSHOLD': 16,'BRAKE': 17,'THROW': 18,'AVOID_ADSB': 19,'GUIDED_NOGPS': 20,'SMART_RTL': 21,'FLOWHOLD': 22,'FOLLOW': 23,'ZIGZAG': 24,'SYSTEMIDLE': 25,'AUTOTUNE': 26,'RALLY': 27}
         self.current_mode = self.get_mode()
+
+        self._v_thread = None
+        self._v_thread_stop_event = threading.Event()
+
 
     def set_mode(self, mode):
         if mode not in self.mode_mapping.keys():
@@ -32,11 +36,10 @@ class Move(Telemetry):
                 if self.mode_mapping[i] == mode_id:
                     current_mode = i
                     break
-
             if current_mode ==  None: raise Exception("mode not found (freddy)")
         return current_mode
 
-    def send_displacement_command_yaw_stay_same(self,mx,my,mz,bitmask=248):
+    def send_displacement_command_yaw_stay_same(self,mx,my,mz,bitmask=4088):
             self.connection.mav.set_position_target_local_ned_send(
                 0,
                 self.connection.target_system,
@@ -49,13 +52,10 @@ class Move(Telemetry):
                 0, 0         # yaw and yaw_rate ignored
                 )
 
-    def send_volocity_command_yaw_stay_same(self,mx,my,mz,bitmask=4088):
-        # TODO: set the right bitmask
-
-        # this command must be sent every 3 seconds to contiu moving
+    def send_volocity_command_yaw_stay_same(self,mx,my,mz,bitmask=int(0b10111000111)):
+        # this command must be sent every 3 seconds to continue moving
         if not bitmask:
             raise ValueError("bit mask not set for volocity command")
-            bitmask = "???" 
 
         self.connection.mav.set_position_target_local_ned_send(
             0,
@@ -69,51 +69,79 @@ class Move(Telemetry):
             0, 0         # TODO: yaw and yaw_rate 
         )
 
-        # 3527 # this woks but will turn to face the directon of travel
-        # 3520 # same as obove  
-        # 3523 #
-        # 3591 #
-        # 3576 #
-        # 0b110111000111 3527
+    def send_e_stop_command(self):
+        self.move_mode = "STOP"
+        old_mode = self.get_mode()
+        self.set_mode("BRAKE")
+        time.sleep(3)
+        self.set_mode(old_mode)
 
-        # 1	✅ Velocity only, yaw stays the same	952	Safest and most common for “move in direction but don’t turn.”
-        # 2	Velocity + yaw control (set yaw absolute or rate)	944	Same as above but lets you use myaw_poss or myaw_rate.
-        # 3	Velocity only, yaw rate active (ignore yaw position)	936	Lets you spin while moving.
-        # 4	Velocity + yaw position only (ignore yaw rate)	940	Keeps facing a heading you send.
-        # 5	Velocity only, ignore yaw completely (very permissive)	4095	Ignores everything, often used when testing or debugging.
-        # 4095
-        # 940
-        # 936
-        # 944  # move weridly
-        # 952 # not work 
+    def send_displacement_command_yaw_stay_same(self,mx,my,mz,bitmask=4088):
+            self.connection.mav.set_position_target_local_ned_send(
+                0,
+                self.connection.target_system,
+                self.connection.target_component,
+                mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
+                bitmask,  # ignore velocity, acceleration, yaw/yaw_rate (position only)
+                mx, my, mz,  # position offsets in meters
+                0, 0, 0,     # velocity ignored
+                0, 0, 0,     # acceleration ignored
+                0, 0         # yaw and yaw_rate ignored
+                )
+    def move_volocity_until_stop_or_max_time(self,direction,max_time,change_yaw=False):
+        self.stop_volocity_command()
 
-        # bit1:PosX, bit2:PosY, bit3:PosZ, bit4:VelX, bit5:VelY, bit6:VelZ, bit7:AccX, bit8:AccY, bit9:AccZ, bit11:yaw, bit12:yaw rate
+        def repeatedly_send_v_command(direction,max_time,change_yaw):
+            dyaw = 3527
+            no_dyaw = 1479 
+            if change_yaw:
+                bitmask = dyaw
+            else:
+                bitmask = no_dyaw
+            start_time = time.time()
+            while time.time() < start_time + max_time and not self._v_thread_stop_event.is_set():
+                print("sending volocity command on thread")
+                self.send_volocity_command_yaw_stay_same(direction[0],direction[1],direction[2],bitmask)
+                time.sleep(0.3)
+                
+        if self._v_thread is None or not self._v_thread.is_alive(): # this will check if none like after init or later is not runing
+            self._v_thread_stop_event.clear()
+            self._v_thread = threading.Thread(target=repeatedly_send_v_command,args=(direction,max_time,change_yaw))
+            self._v_thread.start()
+            print("started sending move command")
 
-    def send_fly_to_point_yaw_change(self,mx,my,mz):
-        bitmask=248
-        
-        self.connection.mav.set_position_target_local_ned_send(
-            0,
+    def stop_volocity_command(self):
+        if self._v_thread and self._v_thread.is_alive():
+            self._v_thread_stop_event.set()
+            self._v_thread.join()
+            self.send_volocity_command_yaw_stay_same(0,0,0)
+            self._v_thread = None 
+
+    def arm(self): # dange UNTESTED
+        # TODO: test in real world
+        self.connection.mav.command_long_send(
             self.connection.target_system,
             self.connection.target_component,
-            mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
-            bitmask,  # ignore velocity, acceleration, yaw/yaw_rate (position only)
-            mx, my, mz,  # position offsets in meters
-            0, 0, 0,     # velocity ignored
-            0, 0, 0,     # acceleration ignored
-            0, 0         # yaw and yaw_rate ignored
+            mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+            0, 1, 0, 0, 0, 0, 0, 0
+        )
+        while True:
+            msg = self.connection.recv_match(type='HEARTBEAT', blocking=True)
+            if msg.base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED:
+                return True
+            time.sleep(1)
+            
+    def arm_and_take_off_to_hight(self,hight): # dange UNTESTED
+        # TODO: test in real world
+            self.arm()
+            self.connection.mav.command_long_send(
+                self.connection.target_system,
+                self.connection.target_component,
+                mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
+                0, 0, 0, 0, 0, 0, 0, hight
             )
 
-    def send_stop_command(self,new=True,old=False):
-        self.move_mode = "STOP"
-        if new:
-            old_mode = self.get_mode()
-            self.set_mode("BRAKE")
-            time.sleep(3)
-            self.set_mode(old_mode)
-        elif old:
-            self.send_volocity_command_yaw_stay_same(0,0,0,0,0)
-        else:
-            raise ValueError("not inputed the choice")
+
+    
 
 move_singleton = Move()
