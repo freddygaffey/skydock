641b171ac0a7be2b146d3d38b915df0ef0a4bfd2
Fred Gaffey <fredgaffey08@gmail.com>
2025-12-11 10:04:20 +1100

    start the fsm

--- software/fsm/template.py
+++ software/fsm/template.py
@@ -1,4 +1,5 @@
 from typing import Protocol
+import time
 
 
 from telemetry import telm_singleton
@@ -7,31 +8,66 @@ from ai import ai_storage_singleton, camera_prams
 from drone_snapshots import drone_telm_stapshot, ground_station_commands
 
 class DroneState(Protocol):
-    def enter(self, drone): ...
+    def enter(self): ...
 
-    def update(self, drone): ...
+    def update(self): ...
 
-    def exit(self, drone): ...
+    def exit(self): ...
+
+class FSM:
+    def __init__(self):
+        self.state = OnGroundState()
+
+class Context:
+    take_off_hight = 2
+    scaning_complete = False
 
 class OnGroundState(DroneState):
-    def enter(self, drone) -> None:
+    def enter(self) -> None:
         print("Drone is on the ground.")
     
-    def update(self, drone) -> None:
+    def update(self):
         if telm_singleton.run_pre_flight_checks() == True and "takeoff" in ground_station_commands.commands[0]:
             try: 
                 hight = int(ground_station_commands.commands[0][-1])
             except ValueError:
                 hight = 2 # defaut take off hight 
+            Context.take_off_hight = hight
             move_singleton.arm_and_take_off_to_hight(hight)
+            return TakeOff()
+        else:
+            time.sleep(3)
+            return None
 
-
-
-    def exit(self, drone) -> None:
+    def exit(self) -> None:
         print("Drone is leaving Ground state.")
 
+class TakeOff(DroneState):
+    def enter(self):
+        move_singleton.arm_and_take_off_to_hight(Context.take_off_hight)
 
-class 
+    def update(self):
+        check_alt = round(drone_telm_stapshot.altitude_rel_home,1) == Context.take_off_hight
+        if check_alt != True:
+            return None
+        elif Context.scaning_complete:
+            return Scaning()
+        else:
+            return SprayFSM()
+        
+    def exit(self):
+        print("takeoff conpleate")
+
+        
+            
+    
+class Scaning(DroneState):
+class RetunToHome(DroneState):
+
+class SprayFSM:
+class SprayFlyToPoint(DroneState):
+class SprayHomeOverWeed(DroneState):
+class SpraySpray(DroneState):
 
 
 
